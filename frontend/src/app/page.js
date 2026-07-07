162bccdd38b1cb99776a2594780d30df0661b5c8
silambarasanstr <simbube2013@gmail.com>
2026-07-08 02:12:42 +0530

    str

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,10 +2,10 @@ import React from "react";
 import { getDemo } from "@/services/demoService";
 
 const Home = async () => {
-  const demoData = await getDemo();
-  console.log(demoData);
+  // const demoData = await getDemo();
+  // console.log(demoData);
 
-  return <div>Dashboard simbu: str xxxxxxxxxxxxxx {demoData.message}</div>;
+  return <div>Dashboard simbu: str xxxxxxxxxxxxxx </div>;
 };
 
 export default Home;
