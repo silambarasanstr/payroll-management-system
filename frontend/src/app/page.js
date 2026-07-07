f7c2d8737ba77f2fd24273313325d8ea72c61ff7
silambarasanstr <simbube2013@gmail.com>
2026-07-08 02:03:05 +0530

    add link

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,10 +2,10 @@ import React from "react";
 import { getDemo } from "@/services/demoService";
 
 const Home = async () => {
-  // const demoData = await getDemo();
-  
+  const demoData = await getDemo();
+  console.log(demoData);
 
-  return <div>Dashboard simbu</div>;
+  return <div>Dashboard simbu: {demoData.message}</div>;
 };
 
 export default Home;
