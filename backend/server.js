3b22a673a3656a8892d636f13bb786785354312c
silambarasanstr <simbube2013@gmail.com>
2026-07-08 07:59:55 +0530

    backend update

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,15 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import { apiLimiter } from "./middleware/rateLimiter.js";
-import productRoutes from "./routes/productRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
 // ✅ Middleware
-const allowedOrigins = [
-  "http://localhost:7001",
-  
-  process.env.CLIENT_URL,
-].filter(Boolean);
+const allowedOrigins = ["http://localhost:7001", process.env.CLIENT_URL].filter(
+  Boolean,
+);
 
 app.use(
   cors({
@@ -39,26 +34,15 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(express.json());
-app.use(apiLimiter);
 
 // Routes
 app.get("/api", (req, res) => {
-  res
-    .status(200)
-    .json({
-      success: true,
-      data: [],
-      message: "Welcome to the Payroll Management System",
-    });
+  res.status(200).json({
+    success: true,
+    data: [],
+    message: "Welcome to the Payroll Management System",
+  });
 });
-
-
-// app.get("/api/employees", (req, res) => {
-//   res.status(200).json({ success: true, data: employees });
-// });
-
-app.use("/api/products", productRoutes);
-app.use("/api/auth", authRoutes);
 
 // Start Server
 app.listen(PORT, () => {
